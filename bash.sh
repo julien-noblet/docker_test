aa3d03c62c3c99c537a6f5b94a34eceacca857b6
julien-noblet <julien.noblet+github@gmail.com>
2015-06-03 14:09:24 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -6,9 +6,8 @@ sudo -u postgres createuser osm
 sudo -u postgres createdb transilien -O osm
 sudo -u postgres psql -d transilien -c 'CREATE EXTENSION hstore; CREATE EXTENSION postgis;'
 
-sudo -u postgres postgres --single -jE <<-EOL
-  GRANT ALL ON DATABASE transilien TO osm;
-EOL
+sudo -u postgres psql -d transilien -c 'GRANT ALL ON DATABASE transilien TO osm;'
+
 #sudo -u postgres psql -f /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql -d gis
 #sudo -u postgres psql -d transilien -c "ALTER TABLE geometry_columns OWNER TO osm; ALTER TABLE spatial_ref_sys OWNER TO osm;"
 /etc/init.d/postgresql restart
