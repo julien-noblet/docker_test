88d8d0704ca8e291668bde97f7ae5f61cf523c2d
julien-noblet <julien.noblet+github@gmail.com>
2015-06-03 17:52:08 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,12 +2,12 @@
 set -e
 /etc/init.d/postgresql restart
 
-sudo -u postgres createuser osm
-sudo -u postgres createdb -O osm transilien
-sudo -u postgres psql -d transilien -c 'CREATE EXTENSION hstore; CREATE EXTENSION postgis;'
-sudo -u postgres psql -d transilien -c 'ALTER TABLE geometry_columns OWNER TO osm; ALTER TABLE spatial_ref_sys OWNER TO osm;'
+sudo -u postgres createuser "$USER"
+sudo -u postgres createdb -O "$USER" "$DB"
+sudo -u postgres psql -d "$DB" -c 'CREATE EXTENSION hstore; CREATE EXTENSION postgis;'
+sudo -u postgres psql -d "$DB" -c 'ALTER TABLE geometry_columns OWNER TO "$USER"; ALTER TABLE spatial_ref_sys OWNER TO "$USER";'
 
-sudo -u postgres psql -d transilien -c 'GRANT ALL ON DATABASE transilien TO osm;'
+sudo -u postgres psql -d "$DB" -c 'GRANT ALL ON DATABASE "$DB" TO "$USER";'
 echo "/etc/postgresql/9.3/main/pg_hba.conf"
 sed s/peer/trust/ /etc/postgresql/9.3/main/pg_hba.conf > /tmp/pg_hba.conf
 mv /tmp/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
@@ -17,7 +17,7 @@ echo "EOF /etc/postgresql/9.3/main/pg_hba.conf"
 #sudo -u postgres psql -d transilien -c "ALTER TABLE geometry_columns OWNER TO osm; ALTER TABLE spatial_ref_sys OWNER TO osm;"
 /etc/init.d/postgresql restart
 
-osm2pgsql -G -U osm -d transilien ile-de-france-latest.osm.pbf --hstore --create
-osm2pgsql -G -U osm -d transilien picardie-latest.osm.pbf --hstore --append
+osm2pgsql -G -U "$USER" -d "$DB" ile-de-france-latest.osm.pbf --hstore --create
+osm2pgsql -G -U "$USER" -d "$DB" picardie-latest.osm.pbf --hstore --append
 
 
