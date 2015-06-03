466225a8e965d50461411cb03ab07c4c104fdfb8
julien-noblet <julien.noblet+github@gmail.com>
2015-06-03 09:32:53 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,9 +3,11 @@
 /etc/init.d/postgresql restart
 sudo -u postgres createuser osm
 sudo -u postgres createdb transilien -O osm
+#sudo -u postgres psql -f /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql -d gis
+#sudo -u postgres psql -d transilien -c "ALTER TABLE geometry_columns OWNER TO osm; ALTER TABLE spatial_ref_sys OWNER TO osm;"
 sudo -u postgres psql transilien <<-EOL
- transilien=# CREATE EXTENSION postgis;
- transilien=# CREATE EXTENSION hstore;
+ CREATE EXTENSION postgis;
+ CREATE EXTENSION hstore;
 EOL
 
 osm2pgsql -G -U osm -d transilien ile-de-france-latest.osm.pbf --hstore --create
