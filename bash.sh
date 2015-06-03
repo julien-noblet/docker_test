3ee9938153e977611992986a20c5cb3a5a6143c6
julien-noblet <julien.noblet+github@gmail.com>
2015-06-03 17:49:45 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -17,7 +17,7 @@ echo "EOF /etc/postgresql/9.3/main/pg_hba.conf"
 #sudo -u postgres psql -d transilien -c "ALTER TABLE geometry_columns OWNER TO osm; ALTER TABLE spatial_ref_sys OWNER TO osm;"
 /etc/init.d/postgresql restart
 
-osm2pgsql -G -U postgres -d transilien ile-de-france-latest.osm.pbf --hstore --create
-osm2pgsql -G -U postgres -d transilien picardie-latest.osm.pbf --hstore --append
+osm2pgsql -G -U osm -d transilien ile-de-france-latest.osm.pbf --hstore --create
+osm2pgsql -G -U osm -d transilien picardie-latest.osm.pbf --hstore --append
 
 
